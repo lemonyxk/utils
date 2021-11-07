7ed0c572b2512404a54504bfff28d0b92823bea5
lemo <lemo@gamstudio.net>
2021-11-07 19:07:16 +0800

    1.add rate lib.

--- go.mod
+++ go.mod
@@ -1,8 +1,14 @@
 module github.com/lemoyxk/utils
 
-go 1.16
+go 1.17
 
 require (
-	github.com/json-iterator/go v1.1.11
-	github.com/mitchellh/mapstructure v1.4.1
+	github.com/json-iterator/go v1.1.12
+	github.com/mitchellh/mapstructure v1.4.2
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac
+)
+
+require (
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 )
---
+++ rate.go
@@ -0,0 +1,62 @@
+/**
+* @program: utils
+*
+* @description:
+*
+* @author: lemo
+*
+* @create: 2021-11-07 18:10
+**/
+
+package utils
+
+import (
+	"sync"
+
+	rate2 "golang.org/x/time/rate"
+)
+
+type rate int
+
+const Rate rate = iota
+
+func (r rate) New() *Limiter {
+	return &Limiter{keys: make(map[string]*Allow)}
+}
+
+type Allow struct {
+	limiter *rate2.Limiter
+}
+
+// func (a *Allow) SetRate(rate int) *Allow {
+// 	a.limiter.SetLimit(rate2.Limit(rate))
+// 	return a
+// }
+
+func (a *Allow) Allow() bool {
+	return a.limiter.Allow()
+}
+
+type Limiter struct {
+	keys   map[string]*Allow
+	global *Allow
+	mux    sync.RWMutex
+}
+
+func (l *Limiter) Global(rate int) *Allow {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	if l.global == nil {
+		l.global = &Allow{limiter: rate2.NewLimiter(rate2.Limit(rate), rate)}
+	}
+	return l.global
+}
+
+func (l *Limiter) Key(key string, rate int) *Allow {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	if l.keys[key] == nil {
+		l.keys[key] = &Allow{limiter: rate2.NewLimiter(rate2.Limit(rate), rate)}
+	}
+	return l.keys[key]
+}

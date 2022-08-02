4c46513cfbc2c1bc96c99af21933a30a1021cd24
lemo <652878191@qq.com>
2022-08-03 07:30:30 +0800

    1.add tar gz method.

--- crypto.go
+++ crypto.go
@@ -11,52 +11,71 @@
 package utils
 
 import (
+	"crypto/hmac"
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/base64"
-	"encoding/hex"
+	"hash"
 )
 
 type crypto int
 
 const Crypto crypto = iota
 
-func (c crypto) Md5(input []byte) string {
+type cs struct {
+	bts []byte
+}
+
+func (c *cs) Bytes() []byte {
+	return c.bts
+}
+
+func (c *cs) String() string {
+	return string(c.bts)
+}
+
+func (c crypto) Md5(input []byte) *cs {
 	var byte16 = md5.Sum(input)
 	var bytes = make([]byte, 16)
 	for i := 0; i < 16; i++ {
 		bytes[i] = byte16[i]
 	}
-	return hex.EncodeToString(bytes)
+	return &cs{bts: bytes}
 }
 
-func (c crypto) Sha1(input []byte) string {
+func (c crypto) Sha1(input []byte) *cs {
 	var sha = sha1.Sum(input)
 	var bytes = make([]byte, 20)
 	for i := 0; i < 20; i++ {
 		bytes[i] = sha[i]
 	}
-	return hex.EncodeToString(bytes)
+	return &cs{bts: bytes}
 }
 
-func (c crypto) Sha256(input []byte) string {
+func (c crypto) Sha256(input []byte) *cs {
 	var sha = sha256.Sum256(input)
 	var bytes = make([]byte, 32)
 	for i := 0; i < 32; i++ {
 		bytes[i] = sha[i]
 	}
-	return hex.EncodeToString(bytes)
+	return &cs{bts: bytes}
 }
 
-func (c crypto) Sha512(input []byte) string {
+func (c crypto) Sha512(input []byte) *cs {
 	var sha = sha512.Sum512(input)
 	var bytes = make([]byte, 64)
 	for i := 0; i < 64; i++ {
 		bytes[i] = sha[i]
 	}
-	return hex.EncodeToString(bytes)
+	return &cs{bts: bytes}
+}
+
+func (c crypto) HmacSha1(fn func() hash.Hash, key, input []byte) *cs {
+	var h = hmac.New(fn, key)
+	h.Write(input)
+	return &cs{bts: h.Sum(nil)}
 }
 
 func (c crypto) Base64Encode(input []byte) string {
